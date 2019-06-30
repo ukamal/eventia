c6f12c2535e428370ef3929023d0a3db8cd7baa1
Md.Kamal Hossen <flkamal2016@gmail.com>
2019-06-30 16:11:30 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ new WOW().init();
     
 });
 
-/********countTimerArea*******/
+/********jqueryCountDownTimer*******/
 function makeTimer() {
 
 	//		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");	
